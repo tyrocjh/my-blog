6a512ca1fac17817acfac2c95e55e2f9904e8d98
tychen <tychen@xogrp.com>
2016-05-18 18:21:37 +0800

    add post get method

--- router/routes/admin/blogInfo.js
+++ router/routes/admin/blogInfo.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var BlogInfo = require('../../../models/blogInfo');
 
 router.get('/', function (req, res) {
-	res.send('get blog info...');
+	BlogInfo.findOne(function(err, model) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.send(model);
+		}
+	});
 });
 
 router.post('/', function (req, res) {
-	res.send('post blog info...');
+	params = req.body;
+	BlogInfo.create(params, function(err, model) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.send(model);
+		}
+	});
 });
 
 module.exports = router;
