55bc9761ad0754c3586e7b7942c9ab0aa84b4938
tychen <tychen@xogrp.com>
2016-05-20 18:14:54 +0800

    tmp findOneAndUpdate method

--- app/components/admin/BlogInfo.js
+++ app/components/admin/BlogInfo.js
@@ -25,6 +25,12 @@ var BlogInfo = React.createClass({
 		  });
 	},
 
+	changeField: function(field, e) {
+		var change = {};
+		change[field] = e.target.value;
+		this.setState(change);
+	},
+
 	handleSubmit: function(e) {
 		e.preventDefault();
 		data = FormValidation(this, [{
@@ -47,40 +53,54 @@ var BlogInfo = React.createClass({
 		]);
 
 		if(data) {
-			// console.info(data);
 			if(this.state.id) {
-				console.info('yes');
+				console.info('PUT');
+				data['_id'] = this.state.id;
 				fetch('/admin/blog_info', {
 				  method: 'PUT',
-				  body: data
-				})
+				  headers: {
+				    'Accept': 'application/json',
+				    'Content-Type': 'application/json'
+				  },
+				  body: JSON.stringify(data)
+				}).then(function(response) {
+					console.info(response);
+				});
 			} else {
-				console.info('no');
+				console.info('POST');
 			}
 		}
 	},
 
 	render: function() {
 		return (
-			<form className="form-horizontal col-sm-6" onSubmit={this.handleSubmit}>
+			<form ref="bloginfoForm" className="form-horizontal col-sm-6" onSubmit={this.handleSubmit}>
 				<div className="form-group">
 					<div className="col-sm-10 col-sm-offset-2"><h3>博客信息管理</h3></div>
 				</div>
 				<div className="form-group">
 					<label htmlFor="title" className="col-sm-2 control-label">标题：</label>
-					<div className="col-sm-10"><input type="text" ref="title" id="title" className="form-control" value={this.state.title} /></div>
+					<div className="col-sm-10">
+						<input type="text" ref="title" id="title" className="form-control" value={this.state.title} onChange={this.changeField.bind(this, 'title')} />
+					</div>
 				</div>
 				<div className="form-group">
 					<label htmlFor="keywords" className="col-sm-2 control-label">关键词：</label>
-					<div className="col-sm-10"><textarea ref="keywords" id="keywords" className="form-control" value={this.state.keywords}></textarea></div>
+					<div className="col-sm-10">
+						<textarea ref="keywords" id="keywords" className="form-control" value={this.state.keywords} onChange={this.changeField.bind(this, 'keywords')}></textarea>
+					</div>
 				</div>
 				<div className="form-group">
 					<label htmlFor="description" className="col-sm-2 control-label">描述：</label>
-					<div className="col-sm-10"><textarea ref="description" id="description" className="form-control" value={this.state.description}></textarea></div>
+					<div className="col-sm-10">
+						<textarea ref="description" id="description" className="form-control" value={this.state.description} onChange={this.changeField.bind(this, 'description')}></textarea>
+					</div>
 				</div>
 				<div className="form-group">
 					<label htmlFor="copyright" className="col-sm-2 control-label">版权：</label>
-					<div className="col-sm-10"><textarea ref="copyright" id="copyright" className="form-control" value={this.state.copyright}></textarea></div>
+					<div className="col-sm-10">
+						<textarea ref="copyright" id="copyright" className="form-control" value={this.state.copyright} onChange={this.changeField.bind(this, 'copyright')}></textarea>
+					</div>
 				</div>
 				<div className="form-group">
 					<div className="col-sm-10 col-sm-offset-2">
--- router/routes/admin/blogInfo.js
+++ router/routes/admin/blogInfo.js
@@ -16,7 +16,8 @@ router.get('/', function (req, res) {
 		if(err) {
 			console.info(err);
 			res.json({
-				status: 'fail'
+				status: 'fail',
+				data: {}
 			});
 		} else {
 			res.json({
@@ -47,9 +48,40 @@ router.post('/', function (req, res) {
 
 router.put('/', function(req, res) {
 	params = req.body;
+	
+	var query = {'title': '333', 'description': 'my description zz', 'keywords': 'my keywords cc', 'copyright': 'my copyright aaa sd', '_id': '573c41c8356a3db93b858386'};
+	var update = {'title': 'zzzzz', 'description': '22222', 'keywords': '33333', 'copyright': '44444', '_id': '573c41c8356a3db93b858386'};
+	var options = {new: true};
 
-	console.info('put...');
-	res.send('aa');
+	// debugger;
+
+	BlogInfo.findOneAndUpdate(req.body, update, options, function(err, model) {
+		debugger;
+	});
+
+	// BlogInfo.findOneAndUpdate(query, update, options, function(err, model) {
+	//   console.info('aaaa');
+	//   debugger;
+	// });
+
+	// BlogInfo.findOneAndUpdate(params, function(err, model) {
+		// debugger;
+		// console.info(err);
+		// console.info(model);
+		// res.send('aaaaa');
+	// 	if(err) {
+	// 		console.info(err);
+	// 		res.json({
+	// 			status: 'fail',
+	// 			data: {}
+	// 		});
+	// 	} else {
+	// 		res.json({
+	// 			status: 'success',
+	// 			data: model
+	// 		});
+	// 	}
+	// });
 });
 
 module.exports = router;
