0573017fbee86713d503981aec8aacb803c1bd0f
tychen <tychen@xogrp.com>
2016-05-27 18:05:46 +0800

    tmp

--- app/components/admin/ArticleTagList.js
+++ app/components/admin/ArticleTagList.js
@@ -6,12 +6,33 @@ var React = require('react'),
 var	Link = ReactRouter.Link;
 
 var ArticleTagList = React.createClass({
+	getInitialState: function() {
+		return {
+		}
+	},
+
+	componentDidMount: function() {
+		// fetch('/admin/blog_info')
+		//   .then(function(response) {
+		//     return response.json();
+		//   }).then(function(json) {
+		//     this.setState({
+		//     	id: json.data._id,
+		//     	title: json.data.title,
+		//     	keywords: json.data.keywords,
+		//     	description: json.data.description,
+		//     	copyright: json.data.copyright
+		//     });
+		//   }.bind(this)).catch(function(ex) {
+		//     console.log('parsing failed', ex);
+		//   });
+	},
+
 	render: function() {
 		return (
-			// table
 			<section class="article-tag-list">
 				<Link to={ADMINPATH + "/articleTag"} className="btn btn-default">新增</Link>
-				<table class="tmp">
+				<table className="table">
 					<thead>
 						<tr><th>序号</th><th>名称</th><th>路径</th><th>操作</th></tr>
 					</thead>
@@ -21,8 +42,8 @@ var ArticleTagList = React.createClass({
 							<td>mingcheng</td>
 							<td>lujing</td>
 							<td>
-								<Link to={ADMINPATH + "/articleTag"} className="btn btn-default">编辑</Link>
-								<a href="#">删除</a>
+								<Link to={ADMINPATH + "/articleTag"}>编辑</Link>&nbsp;&nbsp;
+								<a href="javascript:void(0);">删除</a>
 							</td>
 						</tr>
 					</tbody>
